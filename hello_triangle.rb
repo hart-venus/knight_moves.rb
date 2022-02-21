b13c5ee84ba3d765f681808e6544652bbbc276b3
Ariel Leyva <aleyva1509@gmail.com>
2022-02-20 20:44:31 -0600

    after 10 years of debugging it is ALMMOST DONE

--- hello_triangle.rb
+++ hello_triangle.rb
@@ -61,7 +61,26 @@ Rectangle.new(
   width: BOARD_SIZE,
   height: TILE_SIZE + 20,
   color: [1, 1, 1, 0.85],
-  z: 2
+  z: 3
+)
+
+Rectangle.new(
+  x: 550,
+  y: BOARD_SIZE,
+  width: 160,
+  height: 50,
+  color: BLACK_COLOR,
+  z: 12
+)
+
+Text.new(
+  'Calculate',
+  font: 'Roboto-ThinItalic.ttf',
+  size: 24,
+  color: WHITE_COLOR,
+  x: 560,
+  y: BOARD_SIZE + 20,
+  z: 13
 )
 
 def draw_board
@@ -110,7 +129,43 @@ on :mouse_down do |event|
 
     knight_location = map_coords if knight_location_b != map_coords && event.button == :left
     knight_location_b = map_coords if knight_location != map_coords && event.button == :right
+  elsif event.y > 840 && event.y < 890 && event.x > 550 && event.x < 710
+    p knight_location
+    calculate_moves([[knight_location]], knight_location_b)
   end
+end
+
+def calculate_moves(queue, towards)
+  until queue.empty?
+    current_move_series = queue.pop
+    next unless current_move_series.uniq == current_move_series
+
+    if current_move_series.last == towards
+      p current_move_series
+      break
+    end
+
+    calculate_possible_moves(current_move_series.last).each do |possible_move|
+      new_move = current_move_series + [possible_move]
+      queue.unshift(new_move)
+    end
+  end
+end
+
+def calculate_possible_moves(from)
+  possible_moves = []
+  possible_offsets = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+
+  possible_offsets.each do |offset|
+    move = [from[0] + offset[0], from[1] + offset[1]]
+    possible_moves.append(move) if in_board?(move)
+  end
+
+  possible_moves
+end
+
+def in_board?(spot)
+  return true if spot[0] < 8 && spot[0] > -1 && spot[1] < 8 && spot[1] > -1
 end
 
 def lerp(start, stop, step)
